47dcac0e377976fafee72b25e9284744e1a38dfc
M Mayfield <64070251+mm525@users.noreply.github.com>
2021-12-31 13:51:19 +0000

    Update yieldCurve_program.py

--- yieldCurve_program.py
+++ yieldCurve_program.py
@@ -1,5 +1,3 @@
-                                                  # PROGRAM SETUP #
-
 import numpy as np
 import pandas as pd
 import dash
@@ -8,8 +6,9 @@ from sklearn.decomposition import PCA
 import plotly.graph_objs as go
 from dash import html
 from dash import dcc
-from dash import dash_table as dt                                       
-  
+from dash import dash_table as dt
+
+                                            # PROGRAM SETUP #
 # Read data into a DataFrame
 YC = pd.read_excel(r'/Users/marcusmayfield/Documents/US_Yield_Curve.xlsx')
 
@@ -37,11 +36,18 @@ YCnp = YC_nd.to_numpy()
 
                                     # PRINCIPAL COMPONENTS ANALYSIS # 
 
+# Decompose the covariance into its (orthogonal) principal components
 pca_level = PCA().fit(YC_nd[start:end])    
-    
-level = pca_level.components_[0, :]
-slope = pca_level.components_[1, :]
-curvature = pca_level.components_[2, :]
+
+# Share of the total variance explained by the three main factors (level, slope and curvature)
+fLevel = '%.2f'%round(pca_level.explained_variance_ratio_[0], 2)
+fSlope = '%.2f'%round(pca_level.explained_variance_ratio_[1], 2)
+fCurv = '%.2f'%round(pca_level.explained_variance_ratio_[2], 2)
+
+# Eigenvectors for each of the factors
+level = pca_level.components_[0]
+slope = pca_level.components_[1]
+curvature = pca_level.components_[2]
 
                                         # SUMMARY STATISTICS #
 
@@ -61,7 +67,6 @@ summary_table = pd.DataFrame([s1M, s2M, s3M, s6M, s1Y, s2Y, s3Y, s5Y, s7Y, s10Y,
                              columns = ['Maturity', 'Mean', 'Std Dev', 'CV', 'Min', 'Max', 'Current'])
 
                                             # DASHBOARD #
-  
 # Create Dashboard
 app = dash.Dash()
 
@@ -71,7 +76,6 @@ traces = [go.Scatter(x = maturities, y = YCnp[date], mode = 'lines', name = str(
           for date in dates]
 layout = go.Layout(title = 'Daily Treasury Par Yield Curve', xaxis = {'title': 'Maturity'}, 
                    yaxis = {'title': 'Yield (%)'}, yaxis_range = [0, 2.5])
-
 fig = go.Figure(data = traces, layout = layout)
 
 # Graphs for Principal Components Analysis (PCA)
@@ -97,20 +101,33 @@ app.layout = html.Div(children = [
              ]),
     
     # Dashboard section for YC evolution over the inputted timeframe
-    html.Div([html.H2('US Yield Curve Evolution Over Time', style = {'font-family': 'sans-serif'}),
+    html.Div([html.H2('US Yield Curve Evolution Over Time', style = {'font-family': 'sans-serif',
+                                                                    'textAlign': 'center',}),
               dcc.Graph(id = 'yield-curve', figure = fig)]),
     
     # Dashboard section for PCA
-    html.Div([html.H2('Principal Components Analysis (PCA)', style = {'font-family': 'sans-serif'}),
+    html.Div([html.H2('Principal Components Analysis (PCA)', style = {'font-family': 'sans-serif',
+                                                                     'textAlign': 'center',}),
               html.Div([
                   dcc.Graph(id = 'PCA1', figure = fig_level, style = {'display': 'inline-block'}),
                   dcc.Graph(id = 'PCA2', figure = fig_slope, style = {'display': 'inline-block'}),
-                  dcc.Graph(id = 'PCA3', figure = fig_curvature, style = {'display': 'inline-block'})
+                  dcc.Graph(id = 'PCA3', figure = fig_curvature, style = {'display': 'inline-block'}),
+                  html.Div([
+                      html.H3('Share of Variance Explained by The Different Factors',
+                             style = {'font-family': 'sans-serif', 'padding-left': '2.5%'}),
+                      html.H3(f'Level: {fLevel}',
+                             style = {'font-family': 'sans-serif', 'padding-left': '5%'}),
+                      html.H3(f'Slope: {fSlope}',
+                             style = {'font-family': 'sans-serif', 'padding-left': '5%'}),
+                      html.H3(f'Curvature: {fCurv}',
+                              style = {'font-family': 'sans-serif', 'padding-left': '5%'})
+                  ])
               ])
              ]),
     
     # Dashboard section for summary statistics
-    html.Div([html.H2('Summary Statistics', style = {'font-family': 'sans-serif'}),
+    html.Div([html.H2('Summary Statistics', style = {'font-family': 'sans-serif',
+                                                    'textAlign': 'center',}),
               dt.DataTable(id = 'summary-table',
                            columns = [{'name': i, 'id': i} for i in summary_table.columns],
                            data = summary_table.to_dict('records'),
